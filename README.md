91647ba16dc8280af7f14434f2f45a754d3cf8a6
Madun877 <68345731+Madun877@users.noreply.github.com>
2022-02-08 11:45:08 +0700

    Add files via upload
    Xampp terbaru versi

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Sistem-Informasi
-Sistem Informasi SMK
+# PS
+Aplikasi PS atau merupakan aplikasi Pendataan Siswa merupakan sebuah aplikasi berbasis web yang saya kembangkan, aplikasi ini digunakan untuk mencatat data para murid, dari awal masuk hingga lulus, didalam aplikasi ini juga terdapat pendataan SPP dimana si admin dapat menambah data dari SPP siswa
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aplikasi </title>
+<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
+<script type="text/javascript" src="assets/bootstrap/jquery.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="">Sistem Informasi Siswa Smk Yappa Depok</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#"><span class="fa fa-home">Home</span></a></li>
+				<li><a href="#">Menu</a></li>
+			</ul>
+			    <ul class="nav navbar-nav navbar-right">
+      <li><a href="login.php"><span class="fa fa-sign-in"></span> Login</a></li>
+    </ul>
+		</div>
+	</nav>
+
+	<div class="tengah container">
+		<center><h1>Sistem Informasi Siswa Smk Yappa Depok</h1></center>
+
+		<center><h3>JL. PROKLAMASI GG. MAJLIS NO. 79 SUKMAJAYA, Abadijaya, Kec. Sukmajaya, Kota Depok Prov. Jawa Barat</h3></center>
+	</div>
+
+
+
+	<div class="portal">
+		<h1 align="center">Pilih Menu Dibawah ini</h1>
+	<div class="col-sm-4">
+		<center>
+			<a href="data_sekolah.php">
+				<div class="menu">
+					<h1>Data Sekolah</h1>
+					<br>
+					<img src="assets/img/graduate.png">
+				</div>
+			</a>
+		</center>
+	</div>
+	<div class="col-sm-4">
+		<center>
+			<a href="data_siswa.php">
+				<div class="menu">
+					<h1>Data Siswa</h1>
+					<br>
+					<img src="assets/img/student.png">
+				</div>
+			</a>
+		</center>
+	</div>
+	<div class="col-sm-4">
+		<center>
+			<a href="data_spp.php">
+				<div class="menu">
+					<h1>Data SPP</h1>
+					<img style="margin-top: 12px;" src="assets/img/logo.png">
+				</div>
+			</a>
+		</center>
+	</div>
+	</div>
+	<div class="footer" style="background-color: black; margin-top: 380px; width: 100%; height: 40px; ">
+		<p style="text-align: center; color: white; margin-bottom: 10px !important;">Copyright &copy</p>
+	</div>
+	<!--<div id="footer"><center>Copyright &copy Angga Nur</center></div>-->
+	</body>
+</html>
